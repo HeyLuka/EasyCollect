30e096150d5784d394e4e32d3794aadadf618e17
HeyLuka <heyhippo2013@gmail.com>
2016-04-02 01:31:50 +0200

    employee information added to firebase

--- app/js/countPlatCtrl.js
+++ app/js/countPlatCtrl.js
@@ -1,15 +1,24 @@
-countPlatApp.controller('CountPlatCtrl', function ($scope, $firebaseObject, CountPlat) {
+countPlatApp.controller('CountPlatCtrl', function ($scope, $firebaseArray, $firebaseObject, CountPlat) {
 
-	var ref = new Firebase("https://glowing-heat-1407.firebaseio.com");
+	// var ref = new Firebase("https://glowing-heat-1407.firebaseio.com");
+	var ref = new Firebase("https://countplat.firebaseio.com");
+
+	// ref.child("employeeList").push({"id": 1, "name": "Stacy", "title": "VP", "description": "responsible for business strategy and future business plan"});
+	// ref.child("employeeList").push({"id": 2, "name": "Eric", "title": "PR", "description": "responsible for public resourcing and dealing with emergent events"});
+	// ref.child("employeeList").push({"id": 3, "name": "Wally", "title": "CFO", "description": "responsible for investment and capital funding"});
 
 	// $scope.data = $firebaseObject(ref);
 
 	// download the data into a local object
-	var syncObject = $firebaseObject(ref);
+	// var syncObject = $firebaseObject(ref);
+
+	var syncArray = $firebaseArray(ref.child("employeeList"));
 
 	// synchronize the object with a three-way data binding
 	// click on 'index.html' above to see it used in the DOM
-	syncObject.$bindTo($scope, "data");
+	// syncObject.$bindTo($scope, "data");
+	// syncArray.$bindTo($scope, "arraydata");
+	$scope.message = $firebaseArray(ref.child("employeeList"));
 
 	$scope.numberOfVisitors = CountPlat.getNumberOfVistors();
 
