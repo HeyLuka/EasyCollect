3cd2d67aff32d23350257fb852b22fd07971a6b5
HeyLuka <heyhippo2013@gmail.com>
2016-04-02 10:50:46 +0200

    view part finished

--- app/js/countPlatCtrl.js
+++ app/js/countPlatCtrl.js
@@ -3,9 +3,14 @@ countPlatApp.controller('CountPlatCtrl', function ($scope, $firebaseArray, $fire
 	// var ref = new Firebase("https://glowing-heat-1407.firebaseio.com");
 	var ref = new Firebase("https://countplat.firebaseio.com");
 
+	var testdate = new Date().toLocaleString();
+	console.log(testdate);
+
 	// ref.child("employeeList").push({"id": 1, "name": "Stacy", "title": "VP", "description": "responsible for business strategy and future business plan"});
 	// ref.child("employeeList").push({"id": 2, "name": "Eric", "title": "PR", "description": "responsible for public resourcing and dealing with emergent events"});
 	// ref.child("employeeList").push({"id": 3, "name": "Wally", "title": "CFO", "description": "responsible for investment and capital funding"});
+	// ref.child("talkingList").push({"employeeName": "Wally", "visitorName": "Charlie", "time": new Date().toLocaleString()});
+	// ref.child("talkingList").push({"employeeName": "Eric", "visitorName": "Johnny", "time": new Date().toLocaleString()});
 
 	// $scope.data = $firebaseObject(ref);
 
@@ -20,6 +25,10 @@ countPlatApp.controller('CountPlatCtrl', function ($scope, $firebaseArray, $fire
 	// syncArray.$bindTo($scope, "arraydata");
 	$scope.message = $firebaseArray(ref.child("employeeList"));
 
+	$scope.visitorList = $firebaseArray(ref.child("visitors"));
+
+	$scope.talkingList = $firebaseArray(ref.child("talkingList"));
+
 	// $scope.numberOfVisitors = CountPlat.getNumberOfVisitors();
 	$scope.numberOfVisitors = CountPlat.getNumberOfVisitors();
 
@@ -29,7 +38,7 @@ countPlatApp.controller('CountPlatCtrl', function ($scope, $firebaseArray, $fire
 
 	$scope.getNumberOfVisitors = function() {
 		// return CountPlat.getNumberOfVisitors();
-		return $scope.message.length;
+		return $scope.visitorList.length;
 	}
 
 	$scope.getEmployeeList = function(){
